4361f81ea3a8f0ce4c627a69a34061436752803b
dansana <danielsana60@gmail.com>
2024-10-05 12:18:03 +0300

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import HomeP from "./pages/HomeP";
 import ViewP from "./pages/ViewP";
+import HomeP from "./pages/HomeP";
 
 function App() {
   return (
-    <div>
-      {/* <HomeP/> */}
-      <ViewP/>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomeP />} />
+          <Route path="/add" element={<ViewP />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
